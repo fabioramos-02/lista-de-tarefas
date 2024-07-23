9245085e84f2cdc6d33e03f0257272447abda1ae
Fabio Ramos <fabiowilliam466@gmail.com>
2024-07-23 10:17:36 -0400

    tests

--- static/js/app.js
+++ static/js/app.js
@@ -5,9 +5,16 @@ $(document).ready(function() {
     function renderTasks() {
         $('#taskList').empty();
         tasks.forEach((task, index) => {
+            const checked = task.completed ? 'checked' : '';
+            const textDecoration = task.completed ? 'line-through' : 'none';
             $('#taskList').append(`
                 <li class="list-group-item d-flex justify-content-between align-items-center">
-                    ${task}
+                    <div class="form-check">
+                        <input type="checkbox" class="form-check-input" id="taskCheckbox${index}" ${checked} data-index="${index}">
+                        <label class="form-check-label" for="taskCheckbox${index}" style="text-decoration: ${textDecoration};">
+                            ${task.text}
+                        </label>
+                    </div>
                     <div>
                         <button class="btn btn-warning btn-sm mr-2 edit-btn" data-index="${index}">Editar</button>
                         <button class="btn btn-danger btn-sm delete-btn" data-index="${index}">Deletar</button>
@@ -17,7 +24,8 @@ $(document).ready(function() {
         });
     }
 
-    function addTask(task) {
+    function addTask(taskText) {
+        const task = { text: taskText, completed: false };
         tasks.push(task);
         localStorage.setItem('tasks', JSON.stringify(tasks));
         renderTasks();
@@ -29,17 +37,23 @@ $(document).ready(function() {
         renderTasks();
     }
 
-    function editTask(index, newTask) {
-        tasks[index] = newTask;
+    function editTask(index, newTaskText) {
+        tasks[index].text = newTaskText;
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+        renderTasks();
+    }
+
+    function toggleTaskCompletion(index) {
+        tasks[index].completed = !tasks[index].completed;
         localStorage.setItem('tasks', JSON.stringify(tasks));
         renderTasks();
     }
 
     $('#taskForm').on('submit', function(event) {
         event.preventDefault();
-        const task = $('#taskInput').val();
-        if (task) {
-            addTask(task);
+        const taskText = $('#taskInput').val();
+        if (taskText) {
+            addTask(taskText);
             $('#taskInput').val('');
         }
     });
@@ -51,15 +65,20 @@ $(document).ready(function() {
 
     $('#taskList').on('click', '.edit-btn', function() {
         editIndex = $(this).data('index');
-        const task = tasks[editIndex];
+        const task = tasks[editIndex].text;
         $('#editTaskInput').val(task);
         $('#editModal').modal('show');
     });
 
+    $('#taskList').on('change', '.form-check-input', function() {
+        const index = $(this).data('index');
+        toggleTaskCompletion(index);
+    });
+
     $('#saveEditBtn').on('click', function() {
-        const newTask = $('#editTaskInput').val();
-        if (newTask) {
-            editTask(editIndex, newTask);
+        const newTaskText = $('#editTaskInput').val();
+        if (newTaskText) {
+            editTask(editIndex, newTaskText);
             $('#editModal').modal('hide');
         }
     });
